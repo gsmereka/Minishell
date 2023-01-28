9531b5d999d098a440694242a2691eb46ba091e0
gsmereka <gabrielsmereka200@gmail.com>
2023-01-28 10:11:12 -0300

    att tester

---
+++ tester/0_0_base/headers/structs.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/22 22:34:12 by gsmereka          #+#    #+#             */
+/*   Updated: 2023/01/25 14:06:16 by gsmereka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+typedef struct s_process
+{
+	int		*status;
+	int		*pid;
+}	t_process;
+
+typedef struct s_data
+{
+	t_process	process;
+
+	char		**paths;
+	char		**envp;
+	char		*valgrind_path;
+
+	int			original_stdin;
+	int			original_stdout;
+	int			original_stder;
+
+	int			input_tests_amount;
+
+	int			*input_tests_fd;
+	int			*expected_outputs_fd;
+	int			*user_outputs_fd;
+	int			*user_error_fd;
+
+	char		**input_tests_name;
+	char		**expected_outputs_name;
+	char		**user_outputs_name;
+	char		**user_error_name;
+}	t_data;
+
+#endif
---
+++ tester/0_0_base/src/utils/ft_itoa.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/12 21:55:25 by gsmereka          #+#    #+#             */
+/*   Updated: 2023/01/25 12:44:24 by gsmereka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/tester.h"
+
+static size_t	ft_calculate_str_size(int n, int is_negative);
+static char		*set_str(int is_negative, size_t str_size, int n);
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		is_negative;
+	size_t	str_size;
+
+	if (n <= -2147483648)
+		return (strdup("-2147483648"));
+	if (n == 0)
+		return (strdup("0"));
+	if (n < 0)
+		is_negative = 1;
+	else
+		is_negative = 0;
+	str_size = ft_calculate_str_size(n, is_negative);
+	str = set_str(is_negative, str_size, n);
+	return (str);
+}
+
+static char	*set_str(int is_negative, size_t str_size, int n)
+{
+	size_t		limit;
+	char		*str;
+
+	limit = 0;
+	str = (char *)calloc((str_size + 1), sizeof(char));
+	if (!str)
+		return (NULL);
+	if (is_negative)
+	{
+		limit = 1;
+		str[0] = '-';
+		n = n * (-1);
+	}
+	while (str_size > limit)
+	{
+		str[str_size - 1] = (n % 10) + '0';
+		n = n / 10;
+		str_size--;
+	}
+	return (str);
+}
+
+static size_t	ft_calculate_str_size(int n, int is_negative)
+{
+	size_t	str_size;
+
+	str_size = 0;
+	while (n != 0)
+	{
+		n = n / 10;
+		str_size++;
+	}
+	if (is_negative)
+		str_size++;
+	return (str_size);
+}
