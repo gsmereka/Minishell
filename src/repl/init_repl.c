2f59b219a9cf23a33caed50345051dd0fb3c58a6
gsmereka <gabrielsmereka200@gmail.com>
2023-01-31 12:23:22 -0300

    att repl

--- src/repl/init_repl.c
+++ src/repl/init_repl.c
@@ -6,45 +6,40 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 23:09:03 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/01/30 13:11:39 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/01/31 12:22:21 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-// static void	repl_loop(t_data *data);
+static void	init_loop(t_data *data);
 
 void	init_repl(t_data *data)
 {
-	(void)data;
+	init_loop(data);
 	ft_printf("Hello Mundo\n");
 	exit(0);
 }
 
-// static void	repl_loop(t_data *data)
-// {
-// 	char	*str;
-// 	int		i;
+static void	init_loop(t_data *data)
+{
+	char	*str;
+	int		i;
 
-// 	i = 0;
-// 	(void)data;
-// 	// close(0);
-// 	// str = readline(NULL);
-// 	// // 	// ft_printf("%s\n", str);
-// 	// add_history(str);
-// 	// free(str);
-// 	while (i < 3)
-// 	{
-// 		str = readline("Jose Carlos e Compania:");
-// 		// int	pid;
-
-// 		// pid = fork();
-// 		// if (pid == 0)
-// 		// 	execve("/bin/clear", NULL, data->envp);
-// 		add_history(str);
-// 		// ft_printf("%s\n", str);
-// 		free(str);
-// 		i++;
-// 	}
-// 	rl_clear_history();
-// }
+	i = 0;
+	(void)data;
+	while (1)
+	{
+		str = readline("Hopeshell:$ ");
+		if (!ft_strncmp("exit", str, 4))
+		{
+			free(str);
+			break ;
+		}
+		add_history(str);
+		// ft_printf("%s\n", str);
+		free(str);
+		i++;
+	}
+	rl_clear_history();
+}
