96296599b22230d8fed0617ff83ba67643147bab
gsmereka <gabrielsmereka200@gmail.com>
2023-02-03 12:40:55 -0300

    att repl

--- src/repl/init_repl_signals.c
+++ src/repl/init_repl_signals.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 12:15:30 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/02/03 12:18:10 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/02/03 12:40:24 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ void	init_repl_signals(t_data *data)
 	struct sigaction	ctrl_backsslash;
 
 	(void)data;
+	ft_bzero(&ctrl_c, sizeof(struct sigaction));
+	ft_bzero(&ctrl_backsslash, sizeof(struct sigaction));
+	// sigemptyset(&ctrl_c.sa_mask);
+	// sigemptyset(&ctrl_backsslash.sa_mask);
 	ctrl_c.sa_handler = handle_ctrl_c;
 	ctrl_backsslash.sa_handler = SIG_IGN;
 	sigaction(SIGINT, &ctrl_c, NULL);
