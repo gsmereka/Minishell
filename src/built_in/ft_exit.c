9c407d42a775630af42036f667be68282098846b
gsmereka <gabrielsmereka200@gmail.com>
2023-02-27 12:37:06 -0300

    set execute_built_int

--- src/built_in/ft_exit.c
+++ src/built_in/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:02:50 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/02/20 15:43:15 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/02/27 12:35:05 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 void	ft_exit(char **args, t_data *data)
 {
 	(void)args;
+	free_array_list((void **)args);
 	end_program(data);
 }
