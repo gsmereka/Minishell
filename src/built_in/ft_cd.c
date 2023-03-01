6a986b08ab2456b529651c6d3d9ff76e9f1ec418
gsmereka <gabrielsmereka200@gmail.com>
2023-03-01 01:04:36 -0300

    att cd

--- src/built_in/ft_cd.c
+++ src/built_in/ft_cd.c
@@ -6,20 +6,21 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:01:55 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/02/28 23:30:26 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/01 01:03:17 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-static void	change_dir_envp(t_data *data);
-static int	dir_exist(char *dir);
-static char	*get_pwd(int buffer_size);
-static int	buffer_size_overflow(int buffer_size);
+static t_env	*find_env(char *key, t_data *data);
+static int		dir_exist(char *dir);
+static char		*get_pwd(int buffer_size);
+static int		buffer_size_overflow(int buffer_size);
 
 void	ft_cd(char **args, t_data *data)
 {
 	int		dir_changed;
+	char	*dir;
 
 	if (args[1] && args[2])
 	{
@@ -28,42 +29,43 @@ void	ft_cd(char **args, t_data *data)
 		data->exit_status = 1;
 		return ;
 	}
+	dir = args[1];
 	if (!args[1] || (ft_strlen(args[1]) == 1 && args[1][0] == '~'))
 	{
-		ft_printf("go home\n");
-		data->exit_status = 0;
-		return ;
+		dir = find_env("HOME", data)->value;
 	}
-	if (!dir_exist(args[1])) // verifica se o diretório existe
+	if (!dir_exist(dir)) // verifica se o diretório existe
 		return ;
-	dir_changed = chdir(args[1]); // tenta alterar o diretório atual
+	dir_changed = chdir(dir); // tenta alterar o diretório atual
 	if (dir_changed != -1)
 	{
-		// change_oldpwd
-		change_dir_envp(data); // se alterou, altera a variavel de ambiente no dicionario.
+		if (find_env("OLDPWD", data))
+		{
+			free(find_env("OLDPWD", data)->value);
+			find_env("OLDPWD", data)->value = find_env("PWD", data)->value;
+		}
+		if (find_env("PWD", data))
+		{
+			find_env("PWD", data)->value = get_pwd(1024);
+			if (!find_env("PWD", data)->value)
+				exit_error(12, "Fail at alloc envp value at cd", data);
+		}
 		att_virtual_envp(data); // Tambem atualiza a virtual_envp
 	}
 }
 
-static void	change_dir_envp(t_data *data)
+static t_env	*find_env(char *key, t_data *data)
 {
-	t_env	*pwd;
-	int		buffer_size;
+	t_env	*envp;
 
-	pwd = data->dict_envp;
-	while (pwd && ft_strncmp(pwd->key, "PWD", 4) != 0) // busca a variavel de ambiente PWD
+	envp = data->dict_envp;
+	while (envp) // busca a variavel de ambiente
 	{
-		pwd = pwd->next;
+		if (ft_strncmp(envp->key, key, ft_strlen(key)) == 0)
+			break ;
+		envp = envp->next;
 	}
-	if (pwd)
-	{
-		if (pwd->value)
-			free(pwd->value);
-		buffer_size = 1024;
-		pwd->value = get_pwd(buffer_size); // atualiza o endereço do diretorio atual na variavel de ambiente.
-		if (!pwd->value)
-			exit_error(12, "Fail at alloc pwd value at cd", data);
-	}
+	return (envp);
 }
 
 static int	dir_exist(char *dir) //falta verificar permissões
