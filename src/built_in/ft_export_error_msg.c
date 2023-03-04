ad4f847d535caa4cefed47038ddce833c83c57e3
gsmereka <gabrielsmereka200@gmail.com>
2023-03-04 19:37:53 -0300

    att export_error_msg

--- src/built_in/ft_export_error_msg.c
+++ src/built_in/ft_export_error_msg.c
@@ -6,16 +6,16 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 20:29:03 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/03 20:46:55 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/04 19:37:17 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-void	ft_export_error_msg(int	error, char *name, t_data *data)
+void	ft_export_error_msg(int error, char *name, t_data *data)
 {
 	char		*error_msg;
-	
+
 	if (error == 1)
 	{
 		error_msg = "bash: export: `1=9': not a valid identifier\n";
