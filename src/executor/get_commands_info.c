ac0bf8b7a859d1e305ba768a09f3e101a10e4ec8
gsmereka <gabrielsmereka200@gmail.com>
2023-03-07 20:00:52 -0300

    att executor

--- src/executor/get_commands_info.c
+++ src/executor/get_commands_info.c
@@ -6,36 +6,21 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 13:29:33 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/07 14:15:57 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/07 19:59:46 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-static void	set_input_exec_info(t_data *data);
+static void	get_execution_info(t_data *data);
 static int	count_cmds(t_data *data);
-static void	split_cmd_tokens(t_data *data);
 
 void	get_commands_info(t_data *data)
 {
-	set_input_exec_info(data);
-	split_cmd_tokens(data);
-}
-
-static void	split_cmd_tokens(t_data *data)
-{
-	t_token	*token;
-	t_cmd	**cmd;
-	int		cmd_index;
-
-	cmd_index = 0;
-	token = data->tokens;
-	cmd = data->exec->cmds;
-	while (token)
-	{
-		token = format_cmd(token, cmd[cmd_index], data);
-		cmd_index++;
-	}
+	data->exec = ft_calloc(1, sizeof(t_exec));
+	data->exec->cmds_amount = count_cmds(data);
+	data->exec->cmds = ft_calloc(data->exec->cmds_amount + 1, sizeof(t_cmd));
+	data->exec->pipes = ft_calloc(data->exec->cmds_amount + 1, sizeof(int *));
 }
 
 static int	count_cmds(t_data *data)
@@ -55,12 +40,4 @@ static int	count_cmds(t_data *data)
 		token = token->next;
 	}
 	return (size);
-}
-
-static void	set_input_exec_info(t_data *data)
-{
-	data->exec = ft_calloc(1, sizeof(t_exec));
-	data->exec->cmds_amount = count_cmds(data);
-	data->exec->cmds = ft_calloc(data->exec->cmds_amount + 1, sizeof(t_cmd));
-	data->exec->pipes = ft_calloc(data->exec->cmds_amount + 1, sizeof(int *));
 }
--- src/executor/init_executor.c
+++ src/executor/init_executor.c
@@ -6,13 +6,32 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 13:48:55 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/05 20:22:50 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/07 20:00:08 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
+static void	split_cmd_tokens(t_data *data);
+
 void	init_executor(t_data *data)
 {
 	get_commands_info(data);
+	split_cmd_tokens(data);
+}
+
+static void	split_cmd_tokens(t_data *data)
+{
+	t_token	*token;
+	t_cmd	**cmd;
+	int		cmd_index;
+
+	cmd_index = 0;
+	token = data->tokens;
+	cmd = data->exec->cmds;
+	while (token)
+	{
+		token = format_cmd(token, cmd[cmd_index], data);
+		cmd_index++;
+	}
 }
