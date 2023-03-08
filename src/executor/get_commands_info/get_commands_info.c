43b78b80489b94c54a161f31de8b5f7098f21705
gsmereka <gabrielsmereka200@gmail.com>
2023-03-08 11:01:57 -0300

    att executor

--- src/executor/get_commands_info/get_commands_info.c
+++ src/executor/get_commands_info/get_commands_info.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 13:29:33 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/08 10:02:53 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/08 11:00:28 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,16 @@
 
 static void	get_execution_info(t_data *data);
 static int	count_cmds(t_data *data);
+static void	init_execution_structure(t_data *data);
+static void	set_cmd_tokens(t_data *data);
 
 void	get_commands_info(t_data *data)
+{
+	init_execution_structure(data);
+	set_cmd_tokens(data);
+}
+
+static void	init_execution_structure(t_data *data)
 {
 	data->exec = ft_calloc(1, sizeof(t_exec));
 	data->exec->cmds_amount = count_cmds(data);
@@ -40,4 +48,21 @@ static int	count_cmds(t_data *data)
 		token = token->next;
 	}
 	return (size);
+}
+
+static void	set_cmd_tokens(t_data *data)
+{
+	t_token	*token;
+	t_cmd	**cmd;
+	int		cmd_index;
+
+	cmd_index = 0;
+	token = data->tokens;
+	cmd = data->exec->cmds;
+	while (token)
+	{
+		cmd[cmd_index] = ft_calloc(1, sizeof(t_cmd));
+		token = format_cmd(token, cmd[cmd_index], data);
+		cmd_index++;
+	}
 }
--- src/executor/init_executor.c
+++ src/executor/init_executor.c
@@ -6,35 +6,16 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 13:48:55 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/08 00:56:40 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/08 11:00:33 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
-
-static void	set_cmd_tokens(t_data *data);
 
 void	init_executor(t_data *data)
 {
 	if (!data->tokens)
 		return ;
 	get_commands_info(data);
-	set_cmd_tokens(data);
-}
-
-static void	set_cmd_tokens(t_data *data)
-{
-	t_token	*token;
-	t_cmd	**cmd;
-	int		cmd_index;
-
-	cmd_index = 0;
-	token = data->tokens;
-	cmd = data->exec->cmds;
-	while (token)
-	{
-		cmd[cmd_index] = ft_calloc(1, sizeof(t_cmd));
-		token = format_cmd(token, cmd[cmd_index], data);
-		cmd_index++;
-	}
+	
 }
