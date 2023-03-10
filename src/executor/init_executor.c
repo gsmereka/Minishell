7358086e198765d96371239eec986727007d81de
gsmereka <gabrielsmereka200@gmail.com>
2023-03-10 16:41:24 -0300

    att executor

--- src/executor/init_executor.c
+++ src/executor/init_executor.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 13:48:55 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/10 10:14:45 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/10 16:41:02 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,26 +29,12 @@ void	init_executor(t_data *data)
 
 static int	is_only_one_built_in(t_data *data)
 {
-	char	**args;
+	t_cmd	*first_cmd;
 
 	if (data->exec->cmds_amount > 1)
 		return (0);
-	args = data->exec->cmds[0]->args;
-	if (!args || !args[0])
-		return (0);
-	else if (ft_strncmp(args[0], "echo", ft_strlen(args[0])) == 0)
-		return (1);
-	else if (ft_strncmp(args[0], "cd", ft_strlen(args[0])) == 0)
-		return (1);
-	else if (ft_strncmp(args[0], "env", ft_strlen(args[0])) == 0)
-		return (1);
-	else if (ft_strncmp(args[0], "exit", ft_strlen(args[0])) == 0)
-		return (1);
-	else if (ft_strncmp(args[0], "pwd", ft_strlen(args[0])) == 0)
-		return (1);
-	else if (ft_strncmp(args[0], "export", ft_strlen(args[0])) == 0)
-		return (1);
-	else if (ft_strncmp(args[0], "unset", ft_strlen(args[0])) == 0)
+	first_cmd = data->exec->cmds[0]->args;
+	if (is_built_in(first_cmd))
 		return (1);
 	return (0);
 }
