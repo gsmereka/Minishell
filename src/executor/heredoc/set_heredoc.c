33d9bf2713dc0f6ddd9df2d4dea827b946b05e5b
gsmereka <gabrielsmereka200@gmail.com>
2023-03-14 23:17:03 -0300

    "att heredoc"

--- src/executor/heredoc/set_heredoc.c
+++ src/executor/heredoc/set_heredoc.c
@@ -12,10 +12,69 @@
 
 #include "../../../headers/minishell.h"
 
+static int	alloc_heredoc_pipes(t_data *data);
+static int	setup_individual_heredoc_pipes(t_cmd *cmd);
+static int	alloc_individual_heredoc_pipes(t_cmd *cmd);
+
 int	set_heredoc(t_data *data)
 {
 	heredoc_signals_handling(data);
+	alloc_heredoc_pipes(data);
 	if (!heredoc_cmd_loop(data))
 		return (0);
+	return (1);
+}
+
+static int	alloc_heredoc_pipes(t_data *data)
+{
+	t_cmd	**cmd;
+	int		i;
+
+	i = 0;
+	cmd = data->exec->cmds;
+	if (!cmd)
+		return (0);
+	while (cmd[i])
+	{
+		if (!alloc_individual_heredoc_pipes(cmd[i]))
+			return (0);
+		if (!setup_individual_heredoc_pipes(cmd[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	alloc_individual_heredoc_pipes(t_cmd *cmd)
+{
+	int	size;
+
+	size = 0;
+	if (!cmd->infiles)
+		return (0);
+	while (cmd->infiles[size])
+		size++;
+	cmd->heredocs_pipes = ft_calloc(size + 1, sizeof(int *));
+	if (!cmd->heredocs_pipes)
+		return (0);
+	return (1);
+}
+
+static int	setup_individual_heredoc_pipes(t_cmd *cmd)
+{
+	int	doc;
+
+	doc = 0;
+	if (!cmd->heredocs_pipes)
+		return (0);
+	while (cmd->heredocs_pipes[doc])
+	{
+		cmd->heredocs_pipes[doc] = ft_calloc(2, sizeof(int));
+		if (!cmd->heredocs_pipes[doc])
+			return (0);
+		cmd->heredocs_pipes[doc][0] = -1;
+		cmd->heredocs_pipes[doc][1] = -1;
+		doc++;
+	}
 	return (1);
 }
--- src/signals/heredoc_signals_handling.c
+++ src/signals/heredoc_signals_handling.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 21:08:47 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/14 22:40:27 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/14 23:16:21 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ static void	handle_ctrl_c(int signal)
 {
 	if (signal == SIGINT)
 	{
+		ft_printf("\n");
 		close_heredoc_pipes(g_aux_data);
 		g_aux_data->exec->need_interrupt = 1;
 	}
