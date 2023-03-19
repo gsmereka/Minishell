c4f54bade9aaf9b6458c51fee6d3846ad55155b7
gsmereka <gabrielsmereka200@gmail.com>
2023-03-19 20:55:21 -0300

    att executor

--- src/end_program.c
+++ src/end_program.c
@@ -6,13 +6,15 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:36:43 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/14 19:00:08 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/19 20:52:29 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
 static void	free_data(t_data *data);
+static int	close_all_fds(t_data *data);
+static void	close_files(int *files);
 
 // Saida normal do minishell, atraves do ctrl + D ou do comando exit.
 void	end_program(t_data *data) //comentarios em portugues
@@ -60,4 +62,46 @@ void	free_array_list(void **list)
 		i++;
 	}
 	free(list);
+}
+
+static int	close_all_fds(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	if (!data->exec)
+		return (0);
+	while (i < data->exec->cmds_amount)
+	{
+		if (data->exec->pipes)
+		{
+			if (data->exec->pipes[i][1] != -1)
+				close(data->exec->pipes[i][1]);
+			if (data->exec->pipes[i][0] != -1)
+				close(data->exec->pipes[i][0]);
+		}
+		if (data->exec->cmds)
+		{
+			close_files(data->exec->cmds[i]->infiles_fd);
+			close_files(data->exec->cmds[i]->outfiles_fd);
+		}
+		i++;
+	}
+	return (0);
+}
+
+static void	close_files(int *files)
+{
+	int	i;
+
+	i = 0;
+	if (files)
+	{
+		while (files[i])
+		{
+			if (files[i] != -1)
+				close (files[i]);
+			i++;
+		}
+	}
 }
