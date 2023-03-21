dbf2a90f2a1691181bc9201da4a66be1e0736d9f
gsmereka <gabrielsmereka200@gmail.com>
2023-03-21 20:02:41 -0300

    att echo

--- src/built_in/ft_echo.c
+++ src/built_in/ft_echo.c
@@ -6,47 +6,50 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:02:39 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/14 19:00:08 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/21 20:02:29 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-static void	identify_flags(int *n_flag, char **args);
-static void	display_text_line(int *n_flag, char **args);
+static int	identify_flags(char **args);
+static void	display_text_line(char **args);
 
-// Se o primeiro argumento for -n, ele retira a quebra de linha no final.
 void	ft_echo(char **args, t_data *data)
 {
-	int	n_flag;
+	int		n_flag;
 
-	n_flag = 0;
-	if (!args || !args[1])
+	if (!args)
 		return ;
-	identify_flags(&n_flag, args);
-	display_text_line(&n_flag, args);
+	n_flag = identify_flags(args);
+	if (!n_flag)
+	{
+		display_text_line(args);
+		ft_putstr_fd("\n", 1);
+	}
+	else
+		display_text_line(args + 1);
 }
 
-static void	identify_flags(int *n_flag, char **args)
+static int	identify_flags(char **args)
 {
+	if (!args[1])
+		return (0);
 	if (ft_strncmp(args[1], "-n", 2) == 0)
-		*n_flag = 1;
+		return (1);
+	return (0);
 }
 
-static void	display_text_line(int *n_flag, char **args)
+static void	display_text_line(char **args)
 {
 	int	i;
 
 	i = 1;
-	if (*n_flag)
-		i++;
 	while (args[i])
 	{
 		write(1, args[i], ft_strlen(args[i]));
-		if (args[i + 1])							 // se ainda não for a ultima palavra
+		if (args[i + 1])
 			write(1, " ", 1);
 		i++;
 	}
-	if (*n_flag == 0) 						// se o primeiro argumento for -n
-		write(1, "\n", 1);
 }
