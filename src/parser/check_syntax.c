4f12754b9d799e17528bf3580cefb21fb394ad7d
gsmereka <gabrielsmereka200@gmail.com>
2023-03-24 21:38:42 -0300

    att parser

--- src/parser/check_syntax.c
+++ src/parser/check_syntax.c
@@ -6,12 +6,13 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 19:26:08 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/24 21:00:02 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/24 21:38:26 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
+static int	print_syntax_error_msg(char *token);
 static int	search_errors(t_token *token, t_data *data);
 
 // vou fazer um loop que vai salvando os heredocs, 
@@ -20,8 +21,11 @@ static int	search_errors(t_token *token, t_data *data);
 // passada a lista com heredocs salvos
 int	check_syntax(t_data *data)
 {
-	(void)data;
-	search_errors(data->tokens, data);
+	if (search_errors(data->tokens, data))
+	{
+		att_exit_status(2, data);
+		return (0);
+	}
 	return (1);
 }
 
@@ -31,9 +35,38 @@ static int	search_errors(t_token *token, t_data *data)
 	{
 		if (is_redirect(token, data))
 		{
-			if (token->next)
-				(void)token;
+			if (!token->next)
+				return (print_syntax_error_msg(""));
+			if (is_reserved("<<", token->next))
+				return (print_syntax_error_msg("<<"));
+			if (is_reserved("<", token->next))
+				return (print_syntax_error_msg("<"));
+			if (is_reserved(">>", token->next))
+				return (print_syntax_error_msg(">>"));
+			if (is_reserved(">", token->next))
+				return (print_syntax_error_msg(">"));
 		}
+		if (is_reserved("|", token))
+		{
+			if (!token->next)
+				return (print_syntax_error_msg(""));
+			if (is_reserved("|", token->next))
+				return (print_syntax_error_msg("|"));
+		}
+		token = token->next;
 	}
 	return (0);
+}
+
+static int	print_syntax_error_msg(char *token)
+{
+	char	*msg;
+
+	msg = "bash: syntax error near unexpected token `";
+	if (!*token)
+		token = "newline";
+	msg = ft_strjoin(msg, token);
+	msg = ft_strjoin_with_free(msg, "'\n");
+	ft_putstr_fd(msg, 2);
+	return (1);
 }
--- src/parser/init_parser.c
+++ src/parser/init_parser.c
@@ -6,18 +6,21 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 13:48:55 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/24 20:56:32 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/24 21:38:08 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
 static void	save_heredocs(t_data *data);
+static void	swap_tokens_list(t_token *save, t_data *data);
 
 void	init_parser(t_data *data)
 {
-	check_syntax(data);
-	save_heredocs(data);
+	if (!data->tokens)
+		return ;
+	if (check_syntax(data))
+		save_heredocs(data);
 }
 
 static void	save_heredocs(t_data *data)
@@ -30,24 +33,24 @@ static void	save_heredocs(t_data *data)
 	{
 		if (is_reserved("<<", token))
 		{
-			if (!token->next || !token->next->type)
+			if (token->next && !token->next->type)
 			{
 				add_token(&save, token->content);
 				token->type = ft_strdup("redirect");
 				add_token(&save, token->next->content);
 			}
 			else
-			{
-				// swap_tokens_list(save, data);
 				break ;
-			}
 		}
 		else if (is_reserved("|", token) && !token->next)
-		{
-			// swap_tokens_list(save, data);
 			break ;
-		}
 		token = token->next;
 	}
-	// se não tiver erro de syntax, limpar save
+	swap_tokens_list(save, data);
+}
+
+static void	swap_tokens_list(t_token *save, t_data *data)
+{
+	token_clear(&data->tokens);
+	data->tokens = save;
 }
