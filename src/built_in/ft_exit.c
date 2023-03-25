8954ce8cd2c1e58e5a438c9c0638f1525dd72138
gsmereka <gabrielsmereka200@gmail.com>
2023-03-25 11:55:24 -0300

    att exit fds

--- src/built_in/ft_exit.c
+++ src/built_in/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:02:50 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/22 17:27:05 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/25 11:54:08 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 void	ft_exit(char **args, t_data *data)
 {
 	(void)args;
-	ft_putstr_fd("exit\n", 1);
+	ft_putstr_fd("exit\n", 2);
 	att_exit_status(0, data);
+	close(1);
+	close(0);
 	end_program(data);
 }
