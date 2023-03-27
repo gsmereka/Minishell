e6f7e6bbc8c877d1919f1c2cae10eb2365191cb5
gde-mora <gde-mora@student.42sp.org.br>
2023-03-27 04:13:28 +0200

    quase la, só falta tirar a aspas do final quando n tem

--- src/expander/handle_quotes.c
+++ src/expander/handle_quotes.c
@@ -6,7 +6,7 @@
 /*   By: gde-mora <gde-mora@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 01:05:13 by gde-mora          #+#    #+#             */
-/*   Updated: 2023/03/26 20:52:05 by gde-mora         ###   ########.fr       */
+/*   Updated: 2023/03/27 04:08:32 by gde-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,12 @@ void	expander_envp_value(t_data *data, char **content) //tenho q splitar as aspa
 {
 	char	**mat_content;
 	char	*new_content;
-	char	*aux_content;
 	t_env	*env;
 	int		i;
 	int		flag_env;
 
 	mat_content = ft_split(*content, '$'); //funciona, mas vai ser o último 
 	new_content = NULL;
-	aux_content = NULL;
 	env = data->dict_envp;
 	i = 0;
 	if (*content[0] != '$')
@@ -56,30 +54,6 @@ void	expander_envp_value(t_data *data, char **content) //tenho q splitar as aspa
 				flag_env = 1;
 				break ; 
 			}
-		/*	else if (ft_strlen(mat_content[i]) - 1 > 0)
-			{
-				if (mat_content[i][ft_strlen(mat_content[i]) - 1] == '\'') // e se o len < 0 nessa comparação...
-				{
-					aux_content = malloc(ft_strlen(mat_content[i]) - 1); //para o caso das aspas '$oi' .... mas n resolve '$oi'a
-					int j = 0; //arrumar norma aq
-					while (j < ft_strlen(mat_content[i]) - 1)
-					{
-						aux_content[j] = mat_content[i][j];
-						j++;
-					}
-					aux_content[j] = '\0';
-					if (ft_strcmp(aux_content, env->key) == 0)
-					{
-						free(mat_content[i]);
-						mat_content[i] = ft_strdup(env->value);
-						mat_content[i] = ft_strjoin_gnl(mat_content[i], "'");
-						free(aux_content);
-						aux_content = NULL;
-						flag_env = 1;
-						break ; 
-					}
-				}
-			}*/
 			env = env->next;
 		}
 		if (flag_env == 0)
@@ -101,9 +75,46 @@ void	expander_envp_value(t_data *data, char **content) //tenho q splitar as aspa
 	free_mat(mat_content);
 }
 
-void	expander_quotes_value(t_data *data, **content) //vai splitar por ', mas ele n pode perder as aspas
+void	expander_quotes_value(t_data *data, char **content) //vai splitar por ', mas ele n pode perder as aspas
 {
+	char	**mat_content;
+	char	*new_content;
+	char	*aux_quote;
+	int		i;
 	
+	mat_content = ft_split(*content, '\'');
+	new_content = NULL;
+	aux_quote = NULL;
+	i = 0;
+	while (mat_content[i])
+	{
+		expander_envp_value(data, &mat_content[i]);
+		i++;
+	}
+	i = 0;
+	if (*content[0] == '\'')
+		new_content = ft_strjoin_gnl(new_content, "'");
+	while (mat_content[i])
+	{
+		new_content = ft_strjoin_gnl(new_content, mat_content[i]);
+		new_content = ft_strjoin_gnl(new_content, "'"); //e se n tiver no final? colocar um if antes p o começo tbm
+		i++;
+	}
+/*	if (*content[ft_strlen(*content) - 1] != '\'') //if se for negativo antes!
+	{
+		aux_quote = ft_strdup(ft_strrchr(new_content, '\''));
+		free(*content);
+		*content = ft_strdup(aux_quote);
+		free(new_content);
+		free(aux_quote);
+		free_mat(mat_content);
+		return ;
+		ft_printf("oi\n");
+	}*/
+	free(*content);
+	*content = ft_strdup(new_content);
+	free(new_content);
+	free_mat(mat_content);
 }
 
 void	expander_content_value(t_data *data, char ***content, char **mat_content) //nova func
