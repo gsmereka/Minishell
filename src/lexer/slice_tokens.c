3df3d9be7ca3c5b1a61daea0ff74b52b9af2d8eb
gsmereka <gabrielsmereka200@gmail.com>
2023-03-29 17:58:55 -0300

    att slicer

--- src/lexer/slice_tokens.c
+++ src/lexer/slice_tokens.c
@@ -6,51 +6,53 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 17:45:29 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/29 17:47:54 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/03/29 17:58:12 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
+static void	set_new_token(char *word, int size, t_data *data);
+static int	save_word(char *input, t_data *data);
+
 void	slice_tokens(t_data *data)
 {
-	(void)data;
+	char	*input;
+	int		i;
+
+	i = 0;
+	input = data->user_input;
+	while (input[i])
+	{
+		// i += skip_blankspaces(&input[i], data);
+		// i += save_reserved_word(&input[i], data);
+		i += save_word(&input[i], data);
+	}
 }
 
-// lexer(char *input, t_data *data)
-// {
-// while(input[i])
-// {
-// i += skip_blankspaces(&input[i], data);
-// i += save_reserved_word(&input[i], data);
-// i += save_word(&input[i], data);
-// }
-// }
+static int	save_word(char *input, t_data *data)
+{
+	int	i;
 
-// save_word(char *input)
-// {
-// Int i;
+	i = 0;
+	while (input [i])
+	{
+		// if (input[i] == '\'' || input[i] == '"')
+		// 	i += save_quoted_word(&input[i]);
+		// else if (is_reserved(&input[i + 1]))
+		// 	break ;
+		// else
+			i++;
+	}
+	set_new_token((&input[0]), i, data);
+	return (i);
+}
 
-// i = 0
-// while (input [i])
-// {
-// if (i == '\'' || i == '"')
-// i +=save_quoted_word(&input[i]);
-// else If (is_reserved(&input[i + 1]))
-// break ;
-// else
-// i++;
-// }
-// add_token_2((&input[0]), i, data);
-// return (i);
-// }
+static void	set_new_token(char *word, int size, t_data *data)
+{
+	char	*new_word;
 
-//  add_token_2(char *word, int size, t_data *data)
-// {
-// char *new_word;
-
-// new_word = ft_calloc(size + 1, sizeof(char));
-// ft_strlcpy(new_word, word, size);
-// token_push_back(&data->tokens, new_word);
-// free (new_word);
-// }
+	new_word = ft_calloc(size + 1, sizeof(char));
+	ft_strlcpy(new_word, word, size + 1);
+	add_token(&data->tokens, new_word);
+}
