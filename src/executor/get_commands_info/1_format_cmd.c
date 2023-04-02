373a1ced79efa8d9cd27077c5f7e6380b7bfb340
gsmereka <gabrielsmereka200@gmail.com>
2023-04-01 22:39:11 -0300

    att format_cmd

--- src/executor/get_commands_info/1_format_cmd.c
+++ src/executor/get_commands_info/1_format_cmd.c
@@ -6,13 +6,12 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 14:11:02 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/03/30 17:04:30 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/04/01 22:35:53 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../headers/minishell.h"
 
-static t_token	*find_actual_command(t_token *token);
 static void		set_cmd_composition(t_token *token, t_cmd *cmd);
 static void		set_cmd_args_composition(int args_size,
 					t_token *token, t_cmd *cmd);
@@ -20,11 +19,8 @@ static int		count_args(t_token *token);
 
 void	format_cmd(t_token *token, t_cmd *cmd)
 {
-	t_token	*cmd_token;
-
+	set_cmd_composition(token, cmd);
 	get_files(token, cmd);
-	cmd_token = find_actual_command(token);
-	set_cmd_composition(cmd_token, cmd);
 }
 
 static void	set_cmd_composition(t_token *token, t_cmd *cmd)
@@ -35,11 +31,12 @@ static void	set_cmd_composition(t_token *token, t_cmd *cmd)
 	args_amount = count_args(token);
 	if (!args_amount)
 		return ;
-	cmd->name = ft_strdup(token->content);
 	cmd->args = ft_calloc(args_amount + 1, sizeof(char *));
-	if (!cmd->name || !cmd->args)
+	if (!cmd->args)
 		return ;
 	set_cmd_args_composition(args_amount, token, cmd);
+	if (cmd->args && cmd->args[0])
+		cmd->name = ft_strdup(cmd->args[0]);
 }
 
 static void	set_cmd_args_composition(int args_size, t_token *token, t_cmd *cmd)
@@ -90,24 +87,4 @@ static int	count_args(t_token *token)
 		token = token->next;
 	}
 	return (args_amount);
-}
-
-static t_token	*find_actual_command(t_token *token)
-{
-	while (token)
-	{
-		if (is_reserved("<", token))
-			token = token->next->next;
-		else if (is_reserved("<<", token))
-			token = token->next->next;
-		else if (is_reserved(">", token))
-			return (NULL);
-		else if (is_reserved(">>", token))
-			return (NULL);
-		else if (is_reserved("|", token))
-			return (NULL);
-		else
-			break ;
-	}
-	return (token);
 }
