0bfb65cfe9b9d8454791909c668766ac8528d742
gde-mora <gde-mora@student.42sp.org.br>
2023-04-09 03:08:08 +0200

    problemas $'oi' e $ resolvidos

--- src/expander/expand_envp_value.c
+++ src/expander/expand_envp_value.c
@@ -6,7 +6,7 @@
 /*   By: gde-mora <gde-mora@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 02:44:33 by gde-mora          #+#    #+#             */
-/*   Updated: 2023/04/08 03:09:50 by gde-mora         ###   ########.fr       */
+/*   Updated: 2023/04/09 03:01:54 by gde-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,58 +18,62 @@ void	expand_envp_value(t_data *data, char **content)
 	char	*new_content;
 	t_env	*env;
 	int		i;
-	int		flag_env;
+	int		flag_env; //vai sair
 	char	*aux;
 
-	mat_content = ft_split(*content, '$');
+	mat_content = split_with_char(*content, '$'); //p resolver os $ sem nd(?)
 	if (!mat_content)
 		return ;
 	if (!*mat_content)
 	{
 		free(mat_content);
-	//	*content = ft_strdup("");
 		mat_content = NULL;
+	//	free(*content);
+	//	*content = ft_strdup("");
+		//--isso resolve o $'oi' -- mas tira o $ sozinho (posso colocar um if para isso --tem q ser antes --mas n resolve um echo 'a'$ por ex.) -- mas e o $$? junto no if do $   se for $ ou $$, passa reto no token...?
 		return ;
 	}
-
-	//deletar o $ nos casos $'oi' ...? como?
-	
 	new_content = NULL;
 	aux = NULL;
 	env = data->dict_envp;
 	i = 0;
-	if (*content[0] != '$')
-		i++;
+//	if (*content[0] != '$')
+//		i++;
 	while (mat_content[i])
 	{
-		flag_env = 0;
-		env = data->dict_envp;
-		if (mat_content[i][0] == '?')
+		if (ft_strcmp(mat_content[i], "$\0") == 0)
 		{
-			aux = ft_strdup(find_env("?", data)->value);
-			aux = ft_strjoin_gnl(aux, &mat_content[i][1]);
-			free(mat_content[i]);
-			mat_content[i] = ft_strdup(aux);
-			free(aux);
-			aux = NULL;
-		}
-		else
-		{
-			while (env)
+			flag_env = 0;
+			i++;
+			if (!mat_content[i])
+				break ;
+			if (mat_content[i][0] == '?')
 			{
-				if (ft_strcmp(mat_content[i], env->key) == 0) //usar find env
+				aux = ft_strdup(find_env("?", data)->value);
+				aux = ft_strjoin_gnl(aux, &mat_content[i][1]);
+				free(mat_content[i]);
+				mat_content[i] = ft_strdup(aux);
+				free(aux);
+				aux = NULL;
+			}
+			else
+			{
+				while (env)
+				{
+					if (ft_strcmp(mat_content[i], env->key) == 0) //usar find env
+					{
+						free(mat_content[i]);
+						mat_content[i] = ft_strdup(env->value);
+						flag_env = 1;
+						break ; 
+					}
+					env = env->next;
+				}
+				if (flag_env == 0)
 				{
 					free(mat_content[i]);
-					mat_content[i] = ft_strdup(env->value);
-					flag_env = 1;
-					break ; 
+					mat_content[i] = ft_strdup("");
 				}
-				env = env->next;
-			}
-			if (flag_env == 0)
-			{
-				free(mat_content[i]);
-				mat_content[i] = ft_strdup("");
 			}
 		}
 		i++;
@@ -77,7 +81,10 @@ void	expand_envp_value(t_data *data, char **content)
 	i = 0;
 	while (mat_content[i])
 	{
-		new_content = ft_strjoin_gnl(new_content, mat_content[i]);
+		if (ft_strcmp(mat_content[i], "$") == 0 && !mat_content[i + 1])
+			new_content = ft_strjoin_gnl(new_content, mat_content[i]);
+		else if (ft_strcmp(mat_content[i], "$") != 0)
+			new_content = ft_strjoin_gnl(new_content, mat_content[i]);
 		i++;
 	}
 	free(*content);
