2e5a11738cd5f45974caf481f33a0a1025f49327
gde-mora <gde-mora@student.42sp.org.br>
2023-04-09 04:15:19 +0200

    norma expand envp value ok

--- src/expander/expand_envp_value.c
+++ src/expander/expand_envp_value.c
@@ -6,33 +6,85 @@
 /*   By: gde-mora <gde-mora@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 02:44:33 by gde-mora          #+#    #+#             */
-/*   Updated: 2023/04/09 03:29:42 by gde-mora         ###   ########.fr       */
+/*   Updated: 2023/04/09 04:11:52 by gde-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
+static void	save_content(char ***content, char ***mat_content)
+{
+	int		i;
+	char	*new_content;
+
+	i = 0;
+	new_content = NULL;
+	while ((*mat_content)[i])
+	{
+		if (ft_strcmp((*mat_content)[i], "$") == 0 && !(*mat_content)[i + 1])
+			new_content = ft_strjoin_gnl(new_content, (*mat_content)[i]);
+		else if (ft_strcmp((*mat_content)[i], "$") != 0)
+			new_content = ft_strjoin_gnl(new_content, (*mat_content)[i]);
+		i++;
+	}
+	free(**content);
+	**content = ft_strdup(new_content);
+	free(new_content);
+	free_mat(*mat_content);
+}
+
+static void	save_special_content(t_data *data, char **content)
+{
+	char	*aux;
+
+	aux = ft_strdup(find_env("?", data)->value);
+	aux = ft_strjoin_gnl(aux, &(*content)[1]);
+	free(*content);
+	*content = ft_strdup(aux);
+	free(aux);
+	aux = NULL;
+}
+
+static void	save_normal_content(t_data *data, char **content)
+{
+	t_env	*env;
+
+	env = find_env(*content, data);
+	if (env)
+	{
+		free(*content);
+		*content = ft_strdup(env->value);
+	}
+	else
+	{
+		free(*content);
+		*content = ft_strdup("");
+	}
+}
+
+static int	check_mat(char ***mat_content)
+{
+	if (!*mat_content)
+		return (0);
+	if (!**mat_content)
+	{
+		free(*mat_content);
+		*mat_content = NULL;
+		return (0);
+	}
+	return (1);
+}
+
 void	expand_envp_value(t_data *data, char **content)
 {
 	char	**mat_content;
-	char	*new_content;
-	t_env	*env;
 	int		i;
-	char	*aux;
 
 	mat_content = split_with_char(*content, '$');
-	if (!mat_content)
+	if (!check_mat(&mat_content))
 		return ;
-	if (!*mat_content)
-	{
-		free(mat_content);
-		mat_content = NULL;
-		return ;
-	}
-	new_content = NULL;
-	aux = NULL;
-	i = 0;
-	while (mat_content[i])
+	i = -1;
+	while (mat_content[++i])
 	{
 		if (ft_strcmp(mat_content[i], "$\0") == 0)
 		{
@@ -40,42 +92,10 @@ void	expand_envp_value(t_data *data, char **content)
 			if (!mat_content[i])
 				break ;
 			if (mat_content[i][0] == '?')
-			{
-				aux = ft_strdup(find_env("?", data)->value);
-				aux = ft_strjoin_gnl(aux, &mat_content[i][1]);
-				free(mat_content[i]);
-				mat_content[i] = ft_strdup(aux);
-				free(aux);
-				aux = NULL;
-			}
+				save_special_content(data, &mat_content[i]);
 			else
-			{
-				env = find_env(mat_content[i], data);
-				if (env)
-				{
-					free(mat_content[i]);
-					mat_content[i] = ft_strdup(env->value);
-				}
-				else
-				{
-					free(mat_content[i]);
-					mat_content[i] = ft_strdup("");
-				}
-			}
+				save_normal_content(data, &mat_content[i]);
 		}
-		i++;
 	}
-	i = 0;
-	while (mat_content[i])
-	{
-		if (ft_strcmp(mat_content[i], "$") == 0 && !mat_content[i + 1])
-			new_content = ft_strjoin_gnl(new_content, mat_content[i]);
-		else if (ft_strcmp(mat_content[i], "$") != 0)
-			new_content = ft_strjoin_gnl(new_content, mat_content[i]);
-		i++;
-	}
-	free(*content);
-	*content = ft_strdup(new_content);
-	free(new_content);
-	free_mat(mat_content);
+	save_content(&content, &mat_content);
 }
